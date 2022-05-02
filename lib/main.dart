1561571d566c5f772196b0c80e55a5c505723522
javier martinez garcia <javier.mgarcia02@gmail.com>
2022-05-02 15:52:41 +0200

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-// ignore_for_file: avoid_print, prefer_const_constructors, unused_import
-
+// ignore_for_file: unused_import, prefer_const_constructors, unnecessary_const, sized_box_for_whitespace, use_full_hex_values_for_flutter_colors, unused_local_variable, unused_element, avoid_print, body_might_complete_normally_nullable
+import 'package:fct_javiermartinez/profile_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,11 +15,169 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
+
 /* 
-  TODO list:
-  Step1 : create the main layout of the app (only the UI)
+  Step 1 : create the main layout of the app (only the UI)
+  Step 2 : Login to my firebase account
+  Step 3 : Create a new firebase proyect
+  Step 4 : Add firebase depenedeicies for flutter
+  Step 5 : Init the firebase app
+  Step 6 : Create the login function
+  Step 7 : create a user and test the app
 
 */
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //iniatialze firebase app
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: FutureBuilder(
+      future: _initializeFirebase(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return LoginScreen();
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    ));
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //login
+  static Future<User?> loginUsingEmailPassword(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        print("No user found for that email");
+      }
+    }
+
+    return user;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //textfield controler
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          const Text("MyApp Title",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold)),
+          const Text(
+            "Login to your App",
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 44.0,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 44.0,
+          ),
+          TextField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+                hintText: "User Email",
+                prefixIcon: Icon(Icons.mail, color: Colors.black)),
+          ),
+          const SizedBox(
+            height: 44.0,
+          ),
+          TextField(
+            controller: _passwordController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+                hintText: "User password",
+                prefixIcon: Icon(Icons.lock, color: Colors.black)),
+          ),
+          const SizedBox(
+            height: 12.0,
+          ),
+          const Text(
+            "Don't remember your password?",
+            style: TextStyle(color: Colors.blue),
+          ),
+          const SizedBox(
+            height: 88.0,
+          ),
+          Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                fillColor: const Color(0XFF0069FE),
+                elevation: 0.0,
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0)),
+                onPressed: () async {
+                  User? user = await loginUsingEmailPassword(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                      context: context);
+                  print(user);
+                  if (user != null) {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => ProfileScreen()));
+                  } else {
+                    Fluttertoast.showToast(
+                        msg: "This email or password are incorrect", // message
+                        toastLength: Toast.LENGTH_SHORT, // length
+                        gravity: ToastGravity.BOTTOM, // location
+                        timeInSecForIosWeb: 2,
+                        backgroundColor: Colors.black38,
+                        fontSize: 12.0 // duration
+                        );
+                  }
+                },
+                child: const Text("Login",
+                    style: TextStyle(color: Colors.white, fontSize: 18.0)),
+              ))
+        ],
+      ),
+    );
+  }
+}
