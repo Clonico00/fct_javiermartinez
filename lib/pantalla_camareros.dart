58e2c0af9606b792df2403893e05caf8e534d832
javier martinez garcia <javier.mgarcia02@gmail.com>
2022-05-23 15:14:35 +0200

    pantalla camareros updates

--- lib/pantalla_camareros.dart
+++ lib/pantalla_camareros.dart
@@ -10,10 +10,78 @@ class CamarerosScreen extends StatefulWidget {
 }
 
 class _CamarerosScreenState extends State<CamarerosScreen> {
+  List<Color> _colors = [
+    Color.fromARGB(255, 6, 9, 94),
+    Color.fromARGB(255, 255, 255, 255),
+  ];
+  List<Color> _colorsRV = [
+    Color.fromARGB(255, 255, 255, 255),
+    Color.fromARGB(255, 6, 9, 94),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        toolbarHeight: 50,
+        elevation: 2,
+        shadowColor: Color.fromARGB(255, 255, 255, 255),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10))),
+        title: Center(
+            child: Text("Croissanteria Párraga",
+                style: TextStyle(
+                    color: _colorsRV[1],
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.w900,
+                    fontFamily: 'Comfortaa'))),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 100,
+            alignment: Alignment.center,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 25.0,
+                ),
+                Text("MESA ${index}",
+                    style: TextStyle(
+                        color: _colorsRV[index % 2],
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w900,
+                        fontFamily: 'Comfortaa')),
+                SizedBox(
+                  width: queryData.size.width - 150.0,
+                ),
+                Icon(
+                  Icons.arrow_forward_rounded,
+                  color: _colorsRV[index % 2],
+                  size: 30.0,
+                )
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: _colors[index % 2],
+              boxShadow: [
+                BoxShadow(
+                    color: _colors[index % 2], blurRadius: 2, spreadRadius: 5)
+              ],
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
